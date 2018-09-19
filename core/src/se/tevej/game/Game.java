f56d86eeb4596c9b56be120264ffef7ac66aea62
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-20 00:29:31 +0200

    Add JUnit as Gradle testCompile and compile dependencies.

--- core/src/se/tevej/game/Game.java
+++ core/src/se/tevej/game/Game.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import org.testng.annotations.Test;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
