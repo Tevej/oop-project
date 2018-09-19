239c5612fa70b970ce99bd462f051f0b95cc0372
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-20 00:32:10 +0200

    Remove incorrect testing test.

--- core/src/test/java/se/tevej/game/TravisTestClassTest.java
+++ core/src/test/java/se/tevej/game/TravisTestClassTest.java
@@ -8,10 +8,7 @@ public class TravisTestClassTest {
     @Test
     public void getThree() throws Exception {
         // Should pass!
-
         assertEquals(3, TravisTestClass.getThree());
-        // Should not pass, here to see if gradle/travis is setup properly.
-        assertEquals(4, TravisTestClass.getThree());
     }
 
 }
