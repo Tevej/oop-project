2f965c6b7afaec805ebedfe6d9b82adf552e2135
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-23 18:43:37 +0200

    Add naturalResource component.

---
+++ core/src/se/tevej/game/model/components/NaturalResourceComponent.java
@@ -0,0 +1,22 @@
+package se.tevej.game.model.components;
+
+import com.badlogic.ashley.core.Component;
+import se.tevej.game.exceptions.NotEnoughResourcesException;
+import se.tevej.game.model.resource.ResourceType;
+
+public class NaturalResourceComponent implements Component {
+    private ResourceType type;
+    private float amount;
+
+    public float getAmountLeft() {
+        return amount;
+    }
+
+    public void extractResource(float amount) throws NotEnoughResourcesException {
+        if (this.amount < amount) {
+            throw new NotEnoughResourcesException();
+        }
+
+        this.amount -= amount;
+    }
+}
