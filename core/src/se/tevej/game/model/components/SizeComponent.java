36b23c8666c44fe1cb8e833a5c310d0943da2219
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-23 18:44:45 +0200

    Add size component.

---
+++ core/src/se/tevej/game/model/components/SizeComponent.java
@@ -0,0 +1,13 @@
+package se.tevej.game.model.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class SizeComponent implements Component {
+    int width;
+    int height;
+
+    public BuildingComponent(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
