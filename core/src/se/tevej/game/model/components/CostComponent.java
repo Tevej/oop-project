609caf8a8ab79d68ff168e7881d47d7a557213fb
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-23 20:58:54 +0200

    Add constructor to CostComponent that initializes resources list.

--- core/src/se/tevej/game/model/components/CostComponent.java
+++ core/src/se/tevej/game/model/components/CostComponent.java
@@ -4,11 +4,16 @@ import com.badlogic.ashley.core.Component;
 import se.tevej.game.model.resource.Resource;
 import se.tevej.game.model.resource.ResourceType;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class CostComponent implements Component {
     private List<Resource> resources;
+
+    public CostComponent() {
+        resources = new ArrayList<Resource>();
+    }
 
     public double getCostOfResource(ResourceType type) {
         for (Resource resource : resources) {
