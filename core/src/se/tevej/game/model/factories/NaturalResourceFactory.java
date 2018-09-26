291840de9b70c2894ea968387d6f8966db01757f
Unknown <erikjer@student.chalmers.se>
2018-09-26 17:02:01 +0200

    Add NaturalResourceFactory

---
+++ core/src/se/tevej/game/model/factories/NaturalResourceFactory.java
@@ -0,0 +1,30 @@
+package se.tevej.game.model.factories;
+
+import com.badlogic.ashley.core.Entity;
+import se.tevej.game.model.ashley.EntityManager;
+import se.tevej.game.model.components.NaturalResourceComponent;
+import se.tevej.game.model.components.PositionComponent;
+import se.tevej.game.model.resource.ResourceType;
+
+public class NaturalResourceFactory {
+    public static Entity createNaturalStoneResource(float x, float y, EntityManager engine){
+        Entity stone = engine.createEntity();
+        stone.add(new NaturalResourceComponent(ResourceType.STONE, 1000));
+        stone.add(new PositionComponent(x,y));
+        return stone;
+    }
+
+    public static Entity createNaturalWoodResource(float x, float y, EntityManager engine){
+        Entity wood = engine.createEntity();
+        wood.add(new NaturalResourceComponent(ResourceType.WOOD, 1000));
+        wood.add(new PositionComponent(x,y));
+        return wood;
+    }
+
+    public static Entity createNaturalWaterResource(float x, float y, EntityManager engine){
+        Entity water = engine.createEntity();
+        water.add(new NaturalResourceComponent(ResourceType.WATER, 1000));
+        water.add(new PositionComponent(x,y));
+        return water;
+    }
+}
