8cad468b94807465ba4b61a45e357250c89b20a1
Unknown <erikjer@student.chalmers.se>
2018-09-27 12:53:29 +0200

    Add sizeComponent to NaturalResourceEntities

--- core/src/se/tevej/game/model/factories/NaturalResourceFactory.java
+++ core/src/se/tevej/game/model/factories/NaturalResourceFactory.java
@@ -4,6 +4,7 @@ import com.badlogic.ashley.core.Entity;
 import se.tevej.game.model.ashley.EntityManager;
 import se.tevej.game.model.components.NaturalResourceComponent;
 import se.tevej.game.model.components.PositionComponent;
+import se.tevej.game.model.components.SizeComponent;
 import se.tevej.game.model.resource.ResourceType;
 
 public class NaturalResourceFactory {
@@ -11,6 +12,7 @@ public class NaturalResourceFactory {
         Entity stone = engine.createEntity();
         stone.add(new NaturalResourceComponent(ResourceType.STONE, 1000));
         stone.add(new PositionComponent(x,y));
+        stone.add(new SizeComponent(1,1));
         return stone;
     }
 
@@ -18,6 +20,7 @@ public class NaturalResourceFactory {
         Entity wood = engine.createEntity();
         wood.add(new NaturalResourceComponent(ResourceType.WOOD, 1000));
         wood.add(new PositionComponent(x,y));
+        wood.add(new SizeComponent(1,1));
         return wood;
     }
 
@@ -25,6 +28,7 @@ public class NaturalResourceFactory {
         Entity water = engine.createEntity();
         water.add(new NaturalResourceComponent(ResourceType.WATER, 1000));
         water.add(new PositionComponent(x,y));
+        water.add(new SizeComponent(1,1));
         return water;
     }
 }
