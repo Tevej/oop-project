e11c55b2022fd9abd61105d9da7ef15588ed645e
Jerge <erikjer@student.chalmers.se>
2018-09-27 22:56:59 +0200

    Extend EntitySystem & add deltaTime
    Made sure that the update function extended the System abstract class and also added functionality for deltaTime

--- core/src/se/tevej/game/model/components/GathererComponent.java
+++ core/src/se/tevej/game/model/components/GathererComponent.java
@@ -12,6 +12,12 @@ public class GathererComponent {
         return resourcePerSecond;
     }
 
+    public Resource getGatheredResource(float deltaTime){
+        Resource r = new Resource(resourcePerSecond);
+        r.setAmount(r.getAmount()*deltaTime);
+        return r;
+    }
+
     public void setResourcePerSecond(Resource newSpeed){
         this.resourcePerSecond.setAmount(newSpeed.getAmount());
     }
