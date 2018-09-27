fddf8f08360b4d5f53a82e2bcb0eac57efe39fe0
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-27 15:34:46 +0200

    Restructured project according to lines.

--- core/src/se/tevej/game/exceptions/NotEnoughResourcesException.java
+++
@@ -1,7 +0,0 @@
-package se.tevej.game.exceptions;
-
-public class NotEnoughResourcesException extends Exception {
-    public NotEnoughResourcesException() {
-        super("Trying to extract more resources than is available.");
-    }
-}
