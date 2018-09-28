be44c3f216e695de671826c6a585f886bb8f0f89
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-28 15:22:26 +0200

    Add homecomponent

---
+++ core/src/main/se/tevej/game/model/components/buildings/HomeComponent.java
@@ -0,0 +1,13 @@
+package main.se.tevej.game.model.components.buildings;
+
+import main.se.tevej.game.model.resource.Resource;
+import main.se.tevej.game.model.resource.ResourceType;
+
+public class HomeComponent extends BuildingComponent {
+    private Resource population;
+
+    public HomeComponent() {
+        super(BuildingType.HOME);
+        population = new Resource(0, ResourceType.POPULATION);
+    }
+}
