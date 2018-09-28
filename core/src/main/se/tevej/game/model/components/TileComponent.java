af8983d26cc70c9402ec66c68d495504a97e5ec2
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-28 12:23:30 +0200

    Add tileComponent.occupy.

--- core/src/main/se/tevej/game/model/components/TileComponent.java
+++ core/src/main/se/tevej/game/model/components/TileComponent.java
@@ -18,6 +18,8 @@ public class TileComponent implements Component {
         return occupier;
     }
 
+    public void occupy(Entity occupier) { this.occupier = occupier; }
+
     public boolean isOccupied() {
         return occupier != null;
     }
