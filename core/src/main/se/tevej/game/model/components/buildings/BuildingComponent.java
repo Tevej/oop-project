715f3c07d9ef0efa313322a40d211ae302835a68
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-28 15:23:12 +0200

    Made BuildingComponent abstract.

--- core/src/main/se/tevej/game/model/components/buildings/BuildingComponent.java
+++ core/src/main/se/tevej/game/model/components/buildings/BuildingComponent.java
@@ -2,7 +2,7 @@ package main.se.tevej.game.model.components.buildings;
 
 import com.badlogic.ashley.core.Component;
 
-public class BuildingComponent implements Component {
+public abstract class BuildingComponent implements Component {
     private BuildingType type;
 
     public BuildingComponent(BuildingType type) {
