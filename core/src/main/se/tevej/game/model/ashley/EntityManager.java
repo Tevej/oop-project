be09f97a261f2615a191e5132a691818f7359c0d
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-28 19:42:01 +0200

    add call to entityManager init and add getSignal to entityManager.

--- core/src/main/se/tevej/game/model/ashley/EntityManager.java
+++ core/src/main/se/tevej/game/model/ashley/EntityManager.java
@@ -2,8 +2,11 @@ package main.se.tevej.game.model.ashley;
 
 import com.badlogic.ashley.core.*;
 import com.badlogic.ashley.signals.Signal;
+import main.se.tevej.game.model.components.PositionComponent;
 import main.se.tevej.game.model.components.TileComponent;
 import main.se.tevej.game.model.components.WorldComponent;
+import main.se.tevej.game.model.components.buildings.BuildingComponent;
+import main.se.tevej.game.model.components.buildings.BuildingType;
 import main.se.tevej.game.model.components.buildings.HomeComponent;
 import main.se.tevej.game.model.factories.WorldFactory;
 import main.se.tevej.game.model.systems.BuildBuildingSystem;
@@ -16,9 +19,10 @@ public class EntityManager {
     public EntityManager(){
         ENGINE = new Engine();
         SIGNAL = new Signal<>();
+        init();
     }
 
-    public void init(){
+    public void init() {
         //Add systems here
         //ENGINE.addSystem(new RenderingSystem());
         ENGINE.addSystem(new BuildBuildingSystem());
@@ -30,16 +34,6 @@ public class EntityManager {
                 SIGNAL.add(signalListener.getSignalListener());
             }
         });
-
-        // createWorldEntity also calls CreateTileEntity and adds it to the engine, this might want to be done separately
-        Entity worldEntity = WorldFactory.createWorldEntity(100, 100, this);
-        addEntityToEngine(worldEntity);
-
-        Entity buildHomeBuilding = new Entity();
-        buildHomeBuilding.add(new HomeComponent());
-        buildHomeBuilding.add(worldEntity.getComponent(WorldComponent.class).getTileAt(50, 50).getComponent(TileComponent.class));
-        buildHomeBuilding.add(new SignalComponent(SignalType.BUILDBUILDING));
-        SIGNAL.dispatch(buildHomeBuilding);
     }
 
     public void update(float deltaTime){
@@ -48,6 +42,10 @@ public class EntityManager {
 
     public Entity createEntity(){
         return new Entity();
+    }
+
+    public Signal getSignal() {
+        return SIGNAL;
     }
 
     public void addEntityToEngine(Entity entity){
