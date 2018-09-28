360c63ae96fd846c2c31773f0aed99d0e0e78041
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-28 15:24:33 +0200

    Call BuildBuildingSystem from entityManager.

--- core/src/main/se/tevej/game/model/ashley/EntityManager.java
+++ core/src/main/se/tevej/game/model/ashley/EntityManager.java
@@ -2,7 +2,11 @@ package main.se.tevej.game.model.ashley;
 
 import com.badlogic.ashley.core.*;
 import com.badlogic.ashley.signals.Signal;
+import main.se.tevej.game.model.components.TileComponent;
+import main.se.tevej.game.model.components.WorldComponent;
+import main.se.tevej.game.model.components.buildings.HomeComponent;
 import main.se.tevej.game.model.factories.WorldFactory;
+import main.se.tevej.game.model.systems.BuildBuildingSystem;
 
 public class EntityManager {
 
@@ -17,6 +21,7 @@ public class EntityManager {
     public void init(){
         //Add systems here
         //ENGINE.addSystem(new RenderingSystem());
+        ENGINE.addSystem(new BuildBuildingSystem());
 
         ENGINE.getSystems().forEach(entitySystem -> {
             if(entitySystem instanceof SignalListener){
@@ -29,6 +34,12 @@ public class EntityManager {
         // createWorldEntity also calls CreateTileEntity and adds it to the engine, this might want to be done separately
         Entity worldEntity = WorldFactory.createWorldEntity(100, 100, this);
         addEntityToEngine(worldEntity);
+
+        Entity buildHomeBuilding = new Entity();
+        buildHomeBuilding.add(new HomeComponent());
+        buildHomeBuilding.add(worldEntity.getComponent(WorldComponent.class).getTileAt(50, 50).getComponent(TileComponent.class));
+        buildHomeBuilding.add(new SignalComponent(SignalType.BUILDBUILDING));
+        SIGNAL.dispatch(buildHomeBuilding);
     }
 
     public void update(float deltaTime){
