ee859b0c8d3fd4eab7a4fa65b53000ed3cdd81d1
ericlp <caeric@student.chalmers.se>
2018-10-05 10:26:41 +0200

    Extracted Enum Key into separate file and renamed to TKey

--- core/src/se/tevej/game/input/listenerInterfaces/OnTappedListener.java
+++ core/src/se/tevej/game/input/listenerInterfaces/OnTappedListener.java
@@ -1,7 +1,8 @@
 package se.tevej.game.input.listenerInterfaces;
 
 import se.tevej.game.input.TKeyBoard;
+import se.tevej.game.input.inputEnums.TKey;
 
 public interface OnTappedListener {
-    void onTapped (TKeyBoard keyBoard, TKeyBoard.Key button);
+    void onTapped (TKeyBoard keyBoard, TKey button);
 }
