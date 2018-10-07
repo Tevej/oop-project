0c6270d2d7554f0ec50dd55ed3a1503bf8840283
Jacob Pedersen <jacobsqy@teknik.io>
2018-10-07 20:19:03 +0200

    Fix findbugs warnings

--- core/src/main/se/tevej/game/view/gui/InventoryGui.java
+++ core/src/main/se/tevej/game/view/gui/InventoryGui.java
@@ -5,19 +5,14 @@ import com.badlogic.gdx.Gdx;
 import main.se.tevej.game.model.components.InventoryComponent;
 import main.se.tevej.game.model.resource.ResourceType;
 import main.se.tevej.game.view.rendering.RenderingFactory;
-import main.se.tevej.game.view.rendering.TTexture;
 import main.se.tevej.game.view.rendering.ui.*;
 
-import java.util.EnumMap;
 import java.util.LinkedList;
-import java.util.Map;
 
 public class InventoryGui {
-    private int ImageSize = 32;
     private Entity inventoryEntity;
     private TTable inventoryTable;
     private RenderingFactory renderingFactory;
-    private TLabel textField;
     private LinkedList<InventoryElement> inventoryElements;
 
     public InventoryGui(RenderingFactory renderingFactory, Entity inventoryEntity) {
@@ -27,6 +22,7 @@ public class InventoryGui {
     }
 
     private void create() {
+        int TABLE_HEIGHT = 32;
         inventoryElements = new LinkedList<>();
         inventoryElements.add(new InventoryElement(renderingFactory, findAmountOfResource(ResourceType.WOOD), "wood.jpg", ResourceType.WOOD));
         inventoryElements.add(new InventoryElement(renderingFactory, findAmountOfResource(ResourceType.WATER), "water.jpg", ResourceType.WATER));
@@ -36,12 +32,12 @@ public class InventoryGui {
 
         inventoryTable = renderingFactory.createTable()
                 .x((Gdx.graphics.getWidth() / 2f))
-                .y(Gdx.graphics.getHeight()- ImageSize / 2f)
+                .y(Gdx.graphics.getHeight()- TABLE_HEIGHT / 2f)
                 .grid(inventoryElements.size()*2, 1)
                 .debug(true);
         for (InventoryElement inventoryElement : inventoryElements) {
             inventoryTable.addElement(inventoryElement.getImage()).height(32).width(32);
-            inventoryTable.addElement(inventoryElement.getLabel());
+            inventoryTable.addElement(inventoryElement.getLabel()).height(32).width(50);
         }
     }
 
