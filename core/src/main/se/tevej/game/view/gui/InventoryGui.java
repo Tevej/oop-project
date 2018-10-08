861d1923e53e30236e3c3efebd461232822782a1
Jacob Pedersen <jacobsqy@teknik.io>
2018-10-08 15:34:49 +0200

    Fix an image path (quarry.jpg -> stone.jpg)

--- core/src/main/se/tevej/game/view/gui/InventoryGui.java
+++ core/src/main/se/tevej/game/view/gui/InventoryGui.java
@@ -26,7 +26,7 @@ public class InventoryGui {
         inventoryElements = new LinkedList<>();
         inventoryElements.add(new InventoryElement(renderingFactory, findAmountOfResource(ResourceType.WOOD), "wood.jpg", ResourceType.WOOD));
         inventoryElements.add(new InventoryElement(renderingFactory, findAmountOfResource(ResourceType.WATER), "water.jpg", ResourceType.WATER));
-        inventoryElements.add(new InventoryElement(renderingFactory, findAmountOfResource(ResourceType.STONE), "quarry.jpg", ResourceType.STONE));
+        inventoryElements.add(new InventoryElement(renderingFactory, findAmountOfResource(ResourceType.STONE), "stone.jpg", ResourceType.STONE));
         inventoryElements.add(new InventoryElement(renderingFactory, findAmountOfResource(ResourceType.FOOD), "food.png", ResourceType.FOOD));
         inventoryElements.add(new InventoryElement(renderingFactory, findAmountOfResource(ResourceType.POPULATION), "population.png", ResourceType.POPULATION));
 
