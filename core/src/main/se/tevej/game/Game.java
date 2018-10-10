53c4f46cc63a870b3d5dea6e99d10662aa88ef2d
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-10-11 00:03:59 +0200

    Now calculates deltaTime (also prints framerate)

--- core/src/main/se/tevej/game/Game.java
+++ core/src/main/se/tevej/game/Game.java
@@ -22,85 +22,107 @@ import main.se.tevej.game.view.View;
 import main.se.tevej.game.view.rendering.RenderingFactory;
 
 public class Game extends ApplicationAdapter {
-	private RenderingFactory renderingFactory;
+    private RenderingFactory renderingFactory;
 
-	private EntityManager em;
-	private View view;
-	private TTable table;
-	private InputLibgdxFactory inputLibgdxFactory;
+    private EntityManager em;
+    private View view;
+    private TTable table;
+    private InputLibgdxFactory inputLibgdxFactory;
 
-	private InventoryGui gui;
+    private InventoryGui gui;
 
+    private long lastFrameNanoTime;
+    private long currFrameNanoTime;
+    private float deltaTime;
 
+    private final long BILLION = (1000 * 1000 * 1000);
 
-	@Override
-	public void create () {
-		inputLibgdxFactory = new InputLibgdxFactory();
-		renderingFactory = new RenderingLibgdxFactory();
+    @Override
+    public void create() {
+        inputLibgdxFactory = new InputLibgdxFactory();
+        renderingFactory = new RenderingLibgdxFactory();
+        lastFrameNanoTime = System.nanoTime();
 
-		em = new EntityManager();
-		view = new View(em, renderingFactory);
+        em = new EntityManager();
+        view = new View(em, renderingFactory);
 
         int worldWidth = 100;
         int worldHeight = 100;
         new CameraController(view, inputLibgdxFactory, 0, 0, worldWidth, worldHeight);
 
-		TButton button = renderingFactory.createButton().image("hulk.jpeg").addListener(() -> System.out.println("Hej!"));
-		TSelectableList selectableList = renderingFactory.createSelectableList().items("Glass", "Godis", "Dricka", "Choklad", "Asdf", "Hmmm", "Marabou").addListener(newSelected -> System.out.println("Selected: " + newSelected));
+        TButton button = renderingFactory.createButton().image("hulk.jpeg").addListener(() -> System.out.println("Hej!"));
+        TSelectableList selectableList = renderingFactory.createSelectableList().items("Glass", "Godis", "Dricka", "Choklad", "Asdf", "Hmmm", "Marabou").addListener(newSelected -> System.out.println("Selected: " + newSelected));
 
-		TTextField textField = renderingFactory.createTextField().set("Hej").addListener(value -> {
-			System.out.println("New value of textfield:" + value);
-		});
+        TTextField textField = renderingFactory.createTextField().set("Hej").addListener(value -> {
+            System.out.println("New value of textfield:" + value);
+        });
 
-		TLabel label = renderingFactory.createLabel().text("This is a label");
+        TLabel label = renderingFactory.createLabel().text("This is a label");
 
-		table = renderingFactory.createTable().x((Gdx.graphics.getWidth() / 2f)).y(Gdx.graphics.getHeight() - 200).grid(2, 2).debug(true);
+        table = renderingFactory.createTable().x((Gdx.graphics.getWidth() / 2f)).y(Gdx.graphics.getHeight() - 200).grid(2, 2).debug(true);
 
-		table.addElement(button).width(200).height(50);
-		table.addElement(textField).width(200).height(50);
-		table.addElement(label).width(200).height(200);
-		table.addElement(selectableList).width(200).height(200);
+        table.addElement(button).width(200).height(50);
+        table.addElement(textField).width(200).height(50);
+        table.addElement(label).width(200).height(200);
+        table.addElement(selectableList).width(200).height(200);
 
-		// Look over naming of method / implementation (also adds the world to the engine.)
-		Entity worldEntity = WorldFactory.createWorldEntity(worldWidth, worldHeight, em);
-		Entity inventoryEntity = new Entity();
-		inventoryEntity.add(new InventoryComponent());
-		em.addEntityToEngine(inventoryEntity);
-		em.addEntityToEngine(worldEntity);
+        // Look over naming of method / implementation (also adds the world to the engine.)
+        Entity worldEntity = WorldFactory.createWorldEntity(worldWidth, worldHeight, em);
+        Entity inventoryEntity = new Entity();
+        inventoryEntity.add(new InventoryComponent());
+        em.addEntityToEngine(inventoryEntity);
+        em.addEntityToEngine(worldEntity);
 
-		Entity buildLumbermill = new Entity();
-		buildLumbermill.add(new BuildingComponent(BuildingType.LUMBERMILL));
-		buildLumbermill.add(worldEntity.getComponent(WorldComponent.class).getTileAt(10, 10).getComponent(PositionComponent.class));
-		buildLumbermill.add(worldEntity.getComponent(WorldComponent.class));
-		buildLumbermill.add(new SignalComponent(SignalType.BUILDBUILDING));
-		em.getSignal().dispatch(buildLumbermill);
+        Entity buildLumbermill = new Entity();
+        buildLumbermill.add(new BuildingComponent(BuildingType.LUMBERMILL));
+        buildLumbermill.add(worldEntity.getComponent(WorldComponent.class).getTileAt(10, 10).getComponent(PositionComponent.class));
+        buildLumbermill.add(worldEntity.getComponent(WorldComponent.class));
+        buildLumbermill.add(new SignalComponent(SignalType.BUILDBUILDING));
+        em.getSignal().dispatch(buildLumbermill);
 
-		Entity buildHomeBuilding = new Entity();
-		buildHomeBuilding.add(new BuildingComponent(BuildingType.HOME));
-		buildHomeBuilding.add(worldEntity.getComponent(WorldComponent.class).getTileAt(5, 5).getComponent(PositionComponent.class));
-		buildHomeBuilding.add(worldEntity.getComponent(WorldComponent.class));
-		buildHomeBuilding.add(new SignalComponent(SignalType.BUILDBUILDING));
-		em.getSignal().dispatch(buildHomeBuilding);
+        Entity buildHomeBuilding = new Entity();
+        buildHomeBuilding.add(new BuildingComponent(BuildingType.HOME));
+        buildHomeBuilding.add(worldEntity.getComponent(WorldComponent.class).getTileAt(5, 5).getComponent(PositionComponent.class));
+        buildHomeBuilding.add(worldEntity.getComponent(WorldComponent.class));
+        buildHomeBuilding.add(new SignalComponent(SignalType.BUILDBUILDING));
+        em.getSignal().dispatch(buildHomeBuilding);
 
-		gui = new InventoryGui(renderingFactory, inventoryEntity);
-	}
+        gui = new InventoryGui(renderingFactory, inventoryEntity);
+    }
 
-	@Override
-	public void render () {
-		em.update(1f / 60f);
+    @Override
+    public void render() {
+        calculateDeltaTime();
 
-		Gdx.gl.glClearColor(1, 1,1 ,1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        em.update(deltaTime);
 
-		view.render();
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		table.update(1f / 60f);
-		//table.render();
-		gui.update(1f / 60f);
-		gui.render();
-	}
-	
-	@Override
-	public void dispose () {
-	}
+        view.render();
+
+        table.update(deltaTime);
+        //table.render();
+        gui.update(deltaTime);
+        gui.render();
+    }
+
+    private float printFrameRate;
+
+    private void calculateDeltaTime() {
+        currFrameNanoTime = System.nanoTime();
+        long diff = currFrameNanoTime - lastFrameNanoTime;
+        lastFrameNanoTime = currFrameNanoTime;
+        deltaTime = ((float)diff / (float)BILLION);
+
+        printFrameRate += deltaTime;
+        if (printFrameRate >= 0.1f) {
+            System.out.println("FPS: " + (1 / deltaTime));
+            printFrameRate = 0;
+        }
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
