ee9981e510d4e522864196b03eb062335ce8126e
ericlp <caeric@student.chalmers.se>
2018-10-11 10:52:30 +0200

    Added Comment regarding what an EntityRenderable is

--- core/src/main/se/tevej/game/view/EntityRenderable.java
+++ core/src/main/se/tevej/game/view/EntityRenderable.java
@@ -3,6 +3,10 @@ package main.se.tevej.game.view;
 import com.badlogic.ashley.core.Entity;
 import main.se.tevej.game.view.rendering.TBatchRenderer;
 
+/**
+ *  An EntityRenderable is a way of rendering an certain entity.
+ *  It specifies which sprites to use, and how and where to render them.
+ */
 public interface EntityRenderable {
     void render(float offsetX, float offsetY, TBatchRenderer batchRenderer, Entity entity, int pixelPerTile) throws Exception;
 }
