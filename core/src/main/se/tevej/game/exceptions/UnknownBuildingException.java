4f474c7072f7697b708dbd9f8c19a46e84e892b2
portals <theodor.ang.98@gmail.com>
2018-10-15 00:17:01 +0200

    Removes import that is not used

--- core/src/main/se/tevej/game/exceptions/UnknownBuildingException.java
+++ core/src/main/se/tevej/game/exceptions/UnknownBuildingException.java
@@ -1,7 +1,5 @@
 package main.se.tevej.game.exceptions;
 
-import main.se.tevej.game.model.components.buildings.BuildingType;
-
 public class UnknownBuildingException extends Exception {
     public UnknownBuildingException() {
         super("Unknown building");
--- core/src/main/se/tevej/game/exceptions/UnknownResourceException.java
+++ core/src/main/se/tevej/game/exceptions/UnknownResourceException.java
@@ -1,7 +1,5 @@
 package main.se.tevej.game.exceptions;
 
-import main.se.tevej.game.model.utils.ResourceType;
-
 public class UnknownResourceException extends Exception {
     public UnknownResourceException() {
         super("Unknown resource");
