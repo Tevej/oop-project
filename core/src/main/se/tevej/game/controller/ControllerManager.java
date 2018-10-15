8a0d31acc1b3d9fe6eeb1cbb00fdc37d96ce60ba
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-10-15 22:11:45 +0200

    Fixed time controls.

--- core/src/main/se/tevej/game/controller/ControllerManager.java
+++ core/src/main/se/tevej/game/controller/ControllerManager.java
@@ -20,11 +20,12 @@ public class ControllerManager {
 
     public ControllerManager(ViewManager viewManager, ModelManager modelManager,
                              int worldWidth, int worldHeight,
-                             OrderedInputMultiplexer inputMultiplexer) {
+                             OrderedInputMultiplexer inputMultiplexer,
+                             OnTimeChangeListener timeChangeListener) {
         this.viewManager = viewManager;
         this.modelManager = modelManager;
         initInput(inputMultiplexer);
-        initControllers(worldWidth, worldHeight);
+        initControllers(worldWidth, worldHeight, timeChangeListener);
     }
 
     private void initInput(OrderedInputMultiplexer inputMultiplexer) {
@@ -33,10 +34,10 @@ public class ControllerManager {
         keyBoard = inputFactory.createKeyBoard();
     }
 
-    private void initControllers(int worldWidth, int worldHeight) {
+    private void initControllers(int worldWidth, int worldHeight, OnTimeChangeListener listener) {
         initCamera(worldWidth, worldHeight);
         initConstructor();
-        initTime();
+        initTime(listener);
     }
 
     private void initCamera(int worldWidth, int worldHeight) {
@@ -52,8 +53,9 @@ public class ControllerManager {
         viewManager.getBuildingGui().addSelectedListener(constructor);
     }
 
-    private void initTime() {
-        new TimeController(keyBoard);
+    private void initTime(OnTimeChangeListener listener) {
+        TimeController timeController = new TimeController(keyBoard);
+        timeController.registerOnTimeChange(listener);
     }
 
 }
