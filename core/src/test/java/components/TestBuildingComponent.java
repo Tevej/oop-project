7b405141f364a8d05d533f661c70633a449c928c
ericlp <caeric@student.chalmers.se>
2018-10-15 21:15:17 +0200

    Added test for BuildingComponent

--- core/src/test/java/components/TestBuildingComponent.java
+++ core/src/test/java/components/TestBuildingComponent.java
@@ -1,4 +1,21 @@
 package components;
 
+import static org.junit.Assert.assertEquals;
+
+import main.se.tevej.game.model.components.buildings.BuildingComponent;
+import main.se.tevej.game.model.components.buildings.BuildingType;
+import org.junit.Test;
+
 public class TestBuildingComponent {
+
+    public TestBuildingComponent() {
+        super();
+    }
+
+    @Test
+    public void testMethod() {
+        BuildingComponent buildingC = new BuildingComponent(BuildingType.HOME);
+
+        assertEquals(buildingC.getType(), BuildingType.HOME);
+    }
 }
