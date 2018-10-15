6582e353f9240cdad567adf7ee288e811d3ff6f9
ericlp <caeric@student.chalmers.se>
2018-10-15 21:15:00 +0200

    Added test for HomeComponent

---
+++ core/src/test/java/components/TestHomeComponent.java
@@ -0,0 +1,22 @@
+package components;
+
+import static org.junit.Assert.assertEquals;
+
+import main.se.tevej.game.model.components.buildings.HomeComponent;
+import main.se.tevej.game.model.utils.Resource;
+import main.se.tevej.game.model.utils.ResourceType;
+import org.junit.Test;
+
+public class TestHomeComponent {
+
+    public TestHomeComponent() {
+        super();
+    }
+
+    @Test
+    public void testMethod() {
+        HomeComponent homeC = new HomeComponent();
+
+        assertEquals(homeC.getCurrentPopulation(), 5);
+    }
+}
