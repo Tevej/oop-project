cd002cfbe516ecf878acab7a6e9f6f9ff3a8a64f
portals <theodor.ang.98@gmail.com>
2018-10-21 19:12:56 +0200

    Adds clarification

--- core/src/main/se/tevej/game/controller/screen/DigitScreen.java
+++ core/src/main/se/tevej/game/controller/screen/DigitScreen.java
@@ -2,9 +2,11 @@ package main.se.tevej.game.controller.screen;
 
 public abstract class DigitScreen {
 
+    //Is used in this package.
     @SuppressWarnings("PMD.DefaultPackage")
     /* package */ final ChangeScreen screenChanger;
 
+    //Is used in this package.
     /* package */ DigitScreen(ChangeScreen screenChanger) {
         this.screenChanger = screenChanger;
     }
