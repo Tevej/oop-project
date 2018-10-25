5f8dc463f3b8d42d98454e9760202d12d808c80e
Eric Carlsson <caeric@student.chalmers.se>
2018-10-26 00:58:55 +0200

    checkstyle

--- core/src/main/se/tevej/game/model/entities/AddToEngineListener.java
+++ core/src/main/se/tevej/game/model/entities/AddToEngineListener.java
@@ -3,7 +3,8 @@ package main.se.tevej.game.model.entities;
 import com.badlogic.ashley.core.Entity;
 
 /**
- * It is, to avoid a dependency on modelManager and make it clearer why the modelManager is used, that ModelManager implements this inteface.
+ * This inteface is used to avoid a dependency on modelManager and make it clearer why the
+ * modelManager is used.
  */
 public interface AddToEngineListener {
     void addEntityToEngine(Entity entity);
